4dc55e9a5a1bcfe1ed2c0bb0ec77c8bb95762f1a
Ivan Ou <ivanou@gmail.com>
2020-09-03 10:49:27 -0700

    Refactor view

---
+++ Share/Extensions/ViewExtension.swift
@@ -0,0 +1,47 @@
+//
+//  ViewExtension.swift
+//  Share
+//
+//  Created by Ivan Ou on 9/3/20.
+//  Copyright © 2020 Ivan Ou. All rights reserved.
+//
+
+import UIKit
+
+extension ShareViewController {
+    
+    func expandSettingsView() {
+        self.viewHeight.constant = 240
+        self.settingsView.isHidden = true
+        self.settingsView.alpha = 0
+        
+        // Expand container, then fade
+        UIView.animate(withDuration: 0.3, animations: {
+            self.viewHeight.constant = 490
+            self.settingsView.isHidden = false
+        }, completion: {
+            finished in
+            UIView.animate(withDuration: 0.2) {
+                self.settingsView.alpha = 1
+            }
+        })
+    }
+    
+    func collapseSettingsView() {
+        self.viewHeight.constant = 490
+        self.settingsView.isHidden = false
+        self.settingsView.alpha = 1
+        
+        // Fade, then collapse container
+        UIView.animate(withDuration: 0.2, animations: {
+            self.settingsView.alpha = 0
+        }, completion: {
+            finished in
+            UIView.animate(withDuration: 0.3) {
+                self.viewHeight.constant = 240
+                self.settingsView.isHidden = true
+            }
+        })
+    }
+    
+}
